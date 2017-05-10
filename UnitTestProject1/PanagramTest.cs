12429202ce2a21ced96167825c9ed903cbfabee5
hanna <hlbernard124@gmail.com>
2017-05-10 16:53:03 -0400

    Testing One Two

---
+++ UnitTestProject1/PanagramTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication4;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PanagramTest
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            //Arrange
+            var panagram = new Panagram();//using WebApplication4;
+            //Act
+            var result = panagram.PanagramTest("Hello World!");
+            //Assert
+            Assert.AreEqual("not pangram", result);
+        }
+
+        [TestMethod]
+        public void AlphabetTest()
+        {
+            var panagram = new Panagram();
+            var result = panagram.PanagramTest("abcdefghijklmnopqrstuvwxyz");
+            Assert.AreEqual("pangram", result);
+        }
+
+        [TestMethod]
+        public void TrueTest()
+        {
+            var panagram = new Panagram();
+            var result = panagram.PanagramTest("The quick brown fox jumps over the lazy dog");
+            Assert.AreEqual("pangram", result);
+        }
+
+        [TestMethod]
+        public void TestingOneTwo()
+        {
+            var panagram = new Panagram();
+            var result = panagram.PanagramTest("Not today satan, not today");
+            Assert.AreEqual("not pangram", result);
+        }
+
+        [TestMethod]
+        public void TestingTesting()
+        {
+            var panagram = new Panagram();
+            var result = panagram.PanagramTest("How much you wanna Bet!$5!");
+            Assert.AreEqual("not pangram", result);
+        }
+    }
+}
---
+++ WebApplication4/Panagram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4
+{
+    public class Panagram
+    {
+        public String PanagramTest(String s)
+        {
+            var rv = "";
+            bool newStr = s.ToLower()
+                .Where(c => Char.IsLetter(c))
+                .GroupBy(c => c)
+                .Count() == 26;
+            if (newStr)
+            { rv = "pangram"; }
+            else
+            { rv="not pangram";}
+            return rv;
+        }
+    }
+}
